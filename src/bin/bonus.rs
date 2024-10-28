6ec604208a2b40333860b945b71ab40a26286b23
odkaz <one.destiny.japan@gmail.com>
2024-10-27 17:07:58 -0700

    bonus

--- src/bin/bonus.rs
+++ src/bin/bonus.rs
@@ -1,4 +1,6 @@
 use matrixlib::base_structs::matrix::{Matrix, TMatrix4};
+use std::fs::OpenOptions;
+use std::io::{self, Write};
 
 pub fn projection(fov: f32, aspect: f32, znear: f32, zfar: f32) -> TMatrix4<f32> {
     let mut arr = [[0.; 4]; 4];
@@ -10,7 +12,20 @@ pub fn projection(fov: f32, aspect: f32, znear: f32, zfar: f32) -> TMatrix4<f32>
     Matrix::from(arr)
 }
 
-fn bonus() {
+fn format_matrix(matrix: Vec<Vec<f32>>) -> String {
+    matrix
+        .iter()
+        .map(|row| {
+            row.iter()
+                .map(|&value| format!("{:.7}", value)) // Format to 7 decimal places
+                .collect::<Vec<String>>() // Collect values as strings
+                .join(", ") // Join each row with commas
+        })
+        .collect::<Vec<String>>() // Collect rows as strings
+        .join("\n") // Join each row with a newline
+}
+
+fn bonus() -> io::Result<()> {
     let znear = 0.1;
     let zfar = 100.0;
     let degree = 135.;
@@ -18,11 +33,19 @@ fn bonus() {
     let height = 600.;
     let fov = degree / 360. * std::f32::consts::PI;
     let aspect = height / width;
-    let res = projection(fov, aspect, znear, zfar);
-    println!("{}", res);
+    let res = projection(fov, aspect, znear, zfar).as_vec();
+    let formatted_string = format_matrix(res);
+
+    let mut file = OpenOptions::new()
+        .write(true)
+        .create(true) // Creates the file if it doesn't exist
+        .truncate(true) // Empties the file if it exists
+        .open("src/bin/bonus/proj")?;
+
+    file.write_all(formatted_string.as_bytes())?;
+    Ok(())
 }
 
 fn main() {
-    println!("Running bonus.rs");
-    bonus();
+    bonus().unwrap();
 }
