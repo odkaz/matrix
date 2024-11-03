3a59c2a8e0ec30b5a9a31f9cff4c6b937c93b59e
Kazuma Noda <knoda@z1r10p5.42lyon.fr>
2024-11-03 16:40:21 +0100

    review ex02

--- src/exercises/ex02.rs
+++ src/exercises/ex02.rs
@@ -16,15 +16,47 @@ mod lerp {
     use crate::exercises::ex02::lerp;
 
     #[test]
-    fn test00() {
+    fn test_numbers() {
         assert_eq!(lerp(0., 1., 0.), 0.0);
         assert_eq!(lerp(0., 1., 1.), 1.0);
         assert_eq!(lerp(0., 1., 0.5), 0.5);
         assert_eq!(lerp(21., 42., 0.3), 27.3);
+        assert_eq!(lerp(10., 20., 0.), 10.0);
+        assert_eq!(lerp(10., 20., 1.), 20.0);
+        assert_eq!(lerp(10., 20., 0.5), 15.0);
+        assert_eq!(lerp(-10., 10., 0.5), 0.0);
+        assert_eq!(lerp(5., 15., 0.2), 7.0);
+        assert_eq!(lerp(5., 15., 0.8), 13.0);
+        assert_eq!(lerp(5., 15., 0.3), 8.0);
+        assert_eq!(lerp(-5., 5., 0.5), 0.0);
+    }
+
+    #[test]
+    fn test_vectors() {
         assert_eq!(
             lerp(Vector::from([2., 1.]), Vector::from([4., 2.]), 0.3),
             Vector::from([2.6, 1.3])
         );
+        assert_eq!(
+            lerp(Vector::from([1., 2.]), Vector::from([3., 4.]), 0.5),
+            Vector::from([2., 3.])
+        );
+        assert_eq!(
+            lerp(Vector::from([0., 0.]), Vector::from([10., 10.]), 0.1),
+            Vector::from([1., 1.])
+        );
+        assert_eq!(
+            lerp(Vector::from([5., 5.]), Vector::from([15., 15.]), 0.5),
+            Vector::from([10., 10.])
+        );
+        assert_eq!(
+            lerp(Vector::from([-5., -5.]), Vector::from([5., 5.]), 0.5),
+            Vector::from([0., 0.])
+        );
+    }
+
+    #[test]
+    fn test_matrices() {
         assert_eq!(
             lerp(
                 Matrix::from([[2., 1.], [3., 4.]]),
@@ -32,6 +64,38 @@ mod lerp {
                 0.5
             ),
             Matrix::from([[11., 5.5], [16.5, 22.]])
+        );
+        assert_eq!(
+            lerp(
+                Matrix::from([[1., 2.], [3., 4.]]),
+                Matrix::from([[5., 6.], [7., 8.]]),
+                0.25
+            ),
+            Matrix::from([[2., 3.], [4., 5.]])
+        );
+        assert_eq!(
+            lerp(
+                Matrix::from([[0., 0.], [0., 0.]]),
+                Matrix::from([[10., 10.], [10., 10.]]),
+                0.1
+            ),
+            Matrix::from([[1., 1.], [1., 1.]])
+        );
+        assert_eq!(
+            lerp(
+                Matrix::from([[5., 5.], [5., 5.]]),
+                Matrix::from([[15., 15.], [15., 15.]]),
+                0.5
+            ),
+            Matrix::from([[10., 10.], [10., 10.]])
+        );
+        assert_eq!(
+            lerp(
+                Matrix::from([[-5., -5.], [-5., -5.]]),
+                Matrix::from([[5., 5.], [5., 5.]]),
+                0.5
+            ),
+            Matrix::from([[0., 0.], [0., 0.]])
         );
     }
 }
