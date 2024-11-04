b2233a08117b7c4e11173d2401e09534ec290237
od_kaz <one.destiny.japan@gmail.com>
2024-11-04 15:11:34 +0100

    added comments

--- src/exercises/ex11.rs
+++ src/exercises/ex11.rs
@@ -6,20 +6,25 @@ impl<T: Scalar, const M: usize> TMatrix<T, M> {
     //if the determinant is 0, the matrix collapses the shape to a line or a point
     //if the determinant is negative, the matrix flips the shape
     fn _deter(&self, data: Vec<Vec<T>>, size: usize) -> T {
+        // Base case for 1x1 matrix
         if size == 1 {
             return data[0][0];
         }
+        // Base case for 2x2 matrix
         if size == 2 {
             return (data[0][0] * data[1][1]) - (data[0][1] * data[1][0]);
         }
         let mut res = T::zero();
+        // Iterate over each element of the first row
         for i in 0..size {
             let coef = data[0][i];
             let mut sign = T::one();
+            // Alternate the sign for cofactors
             if i % 2 == 1 {
                 sign = -T::one();
             }
             let mut vec2d = Vec::new();
+            // Create the submatrix by excluding the current row and column
             for o in 1..size {
                 let mut v = Vec::new();
                 for l in 0..size {
@@ -29,13 +34,16 @@ impl<T: Scalar, const M: usize> TMatrix<T, M> {
                 }
                 vec2d.push(v);
             }
+            // Recursive call to _deter for the submatrix
             let rhs = coef * sign * self._deter(vec2d, size - 1);
+            // Add the result to the determinant
             res = res + rhs;
         }
         res
     }
 
     pub fn determinant(&mut self) -> T {
+        //Recursive call to _deter
         self._deter(self.data.clone(), M)
     }
 }
